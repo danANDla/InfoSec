3e4457666bc07f8da4fc4e8e40976a82dd5010de
archandla <d.a.n.202021@gmail.com>
2023-10-17 00:46:41 +0300

    2.10: add report

--- labs/l2.10_elliptic_curves_digital_signature_algorithm/ecdsa_checker/src/main/kotlin/SignatureChecker.kt
+++ labs/l2.10_elliptic_curves_digital_signature_algorithm/ecdsa_checker/src/main/kotlin/SignatureChecker.kt
@@ -11,10 +11,10 @@ class SignatureChecker(
     private val verbose: Boolean
 ) {
     fun checkSignature(signa: Pair<Long, Long>): Boolean {
-        if(verbose) println("\n---------------------------------")
+        printAllPoints(basePoint)
+        if (verbose) println("\n---------------------------------")
         if (signa.first < 1 || signa.first >= curveFactor || signa.second < 1 || signa.second >= curveFactor) return false
 
-        val v = invMod(signa.second, curveFactor)
         val u1 = e % curveFactor * invMod(signa.second, curveFactor)
         val u2 = signa.first * invMod(signa.second, curveFactor)
 
@@ -30,8 +30,21 @@ class SignatureChecker(
         if (verbose)
             println(String.format("(%d,%d) + (%d,%d) = (%d,%d)", p1.x, p1.y, p2.x, p2.y, resPoint.x, resPoint.y))
 
-        if(verbose) println("---------------------------------\n")
+        if (verbose) println("---------------------------------\n")
         return resPoint.x % curveFactor == signa.first
+    }
+
+    private fun printAllPoints(point: EllipticCurvePoint){
+        var i = 0
+        i++
+        print(String.format("%2d. (%d,%d)", i, point.x, point.y))
+
+        var p = pointAddition(point, basePoint, primeNumber, a)
+        while(p != point){
+            i++
+            print(String.format("%2d. (%d,%d)", i, p.x, p.y))
+            p = pointAddition(p, basePoint, primeNumber, a)
+        }
     }
 
     private fun multiplyPoint(point: EllipticCurvePoint, m: Long): EllipticCurvePoint {
@@ -76,12 +89,13 @@ class SignatureChecker(
             val y3 = mod((lambda * (p1.x - x3) - p1.y), primeNumber)
             return EllipticCurvePoint(x3, y3, false)
         }
+
         fun pointSub(
             p1: EllipticCurvePoint,
             p2: EllipticCurvePoint,
             primeNumber: Long,
             a: Long
-        ): EllipticCurvePoint{
+        ): EllipticCurvePoint {
             if (p1.isEq(p2)) return EllipticCurvePoint(0, 0, true)
             return pointAddition(EllipticCurvePoint(p2.x, -p2.y, false), p1, primeNumber, a)
         }
@@ -91,6 +105,7 @@ class SignatureChecker(
             var nB2: BigInteger = BigInteger.valueOf(divisor)
             return nB1.mod(nB2).longValueExact()
         }
+
         private fun invMod(
             n1: Long,
             n2: Long
