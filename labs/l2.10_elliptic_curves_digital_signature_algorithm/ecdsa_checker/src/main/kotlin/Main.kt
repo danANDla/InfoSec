fb0a8da7ba9292f1d753d41769754ebb11976bcd
archandla <d.a.n.202021@gmail.com>
2023-10-16 20:07:15 +0300

    2.10: using point operations from 2.6

--- labs/l2.10_elliptic_curves_digital_signature_algorithm/ecdsa_checker/src/main/kotlin/Main.kt
+++ labs/l2.10_elliptic_curves_digital_signature_algorithm/ecdsa_checker/src/main/kotlin/Main.kt
@@ -1,17 +1,33 @@
+import java.io.File
+import kotlin.math.sign
+
 fun main(args: Array<String>) {
     doMain(args)
 }
 
-fun doMain(args: Array<String>){
+fun doMain(args: Array<String>) {
+    val signaText = File(args[0]).readText().split("\n")
+    val e = signaText[0].toLong()
+    val publicPointStr = signaText[1].split(",")
+    val signaStr = signaText[2].split(",")
+
+    val signa: Pair<Long, Long> = Pair(signaStr[0].toLong(), signaStr[1].toLong())
+    val publicPoint = EllipticCurvePoint(publicPointStr[0].toLong(), publicPointStr[1].toLong(), false)
+    println(String.format("e=%d, Q=(%d,%d)", e, publicPoint.x, publicPoint.y))
+
+
     val ecdsa = SignatureChecker(
-        EllipticCurvePoint(384,475, false),
-        EllipticCurvePoint(384,276,false),
+        EllipticCurvePoint(562, 89, false),
+        publicPoint,
         751,
         13,
-        12,
+        e,
         -1,
         1,
-        false
+        true
     )
-    ecdsa.checkSignature(Pair(11,9))
+
+    var ans = "incorrect"
+    if(ecdsa.checkSignature(signa)) ans = "CORRECT"
+    println(String.format("signature (%d,%d) is %s", signa.first, signa.second, ans))
 }
--- labs/l2.10_elliptic_curves_digital_signature_algorithm/ecdsa_checker/src/main/kotlin/SignatureChecker.kt
+++ labs/l2.10_elliptic_curves_digital_signature_algorithm/ecdsa_checker/src/main/kotlin/SignatureChecker.kt
@@ -1,51 +1,37 @@
+import java.math.BigInteger
+
 class SignatureChecker(
     private val basePoint: EllipticCurvePoint,
-    private val secretPoint: EllipticCurvePoint,
+    private val publicPoint: EllipticCurvePoint,
     private val primeNumber: Long,
-    private val n: Long,
+    private val curveFactor: Long,
     private val e: Long,
     private val a: Long,
     private val b: Long,
     private val verbose: Boolean
 ) {
     fun checkSignature(signa: Pair<Long, Long>): Boolean {
-        if (signa.first < 1 || signa.first >= n || signa.second < 1 || signa.second >= n) return false
+        if(verbose) println("\n---------------------------------")
+        if (signa.first < 1 || signa.first >= curveFactor || signa.second < 1 || signa.second >= curveFactor) return false
 
-        val v = invMod(signa.second, n)
-        val u1 = (e * v) % n
-        val u2 = (signa.first * v) % n
+        val v = invMod(signa.second, curveFactor)
+        val u1 = e % curveFactor * invMod(signa.second, curveFactor)
+        val u2 = signa.first * invMod(signa.second, curveFactor)
 
         val p1 = multiplyPoint(basePoint, u1)
-        val p2 = multiplyPoint(secretPoint, u2)
-        val resPoint = pointAddition(p1, p2, primeNumber, a)
-        println(String.format("(%d,%d)", resPoint.x, resPoint.y))
-//
-        val checkMultiplying = multiplyPoint(EllipticCurvePoint(384, 475, false), 7)
-        println(String.format("checkMultiplying=(%d,%d)", checkMultiplying.x, checkMultiplying.y))
+        if (verbose)
+            println(String.format("(%d,%d) * %d = (%d, %d)", basePoint.x, basePoint.y, u1, p1.x, p1.y))
 
-        return resPoint.x % n == signa.first
-    }
+        val p2 = multiplyPoint(publicPoint, u2)
+        if (verbose)
+            println(String.format("(%d,%d) * %d = (%d, %d)", publicPoint.x, publicPoint.y, u2, p2.x, p2.y))
 
-    private fun gcdex(
-        n1: Long,
-        n2: Long,
-    ): Pair<Long, Pair<Long, Long>> {
-        if (n1 == 0L) {
-            return Pair(n2, Pair(0L, 1L))
-        }
-        val d = gcdex(n2 % n1, n1)
-        val x1 = d.second.first
-        val y1 = d.second.second
-        return Pair(d.first, Pair(y1 - (n2 / n1) * x1, x1))
-    }
+        val resPoint = pointAddition(p2, p1, primeNumber, a)
+        if (verbose)
+            println(String.format("(%d,%d) + (%d,%d) = (%d,%d)", p1.x, p1.y, p2.x, p2.y, resPoint.x, resPoint.y))
 
-    private fun invMod(
-        n1: Long,
-        n2: Long
-    ): Long {
-        val g = gcdex(n1, n2)
-        if (g.first != 1L && g.first != -1L) throw Exception("No solution for Diophantine")
-        return (g.second.first % n2 + n2) % n2
+        if(verbose) println("---------------------------------\n")
+        return resPoint.x % curveFactor == signa.first
     }
 
     private fun multiplyPoint(point: EllipticCurvePoint, m: Long): EllipticCurvePoint {
@@ -55,41 +41,63 @@ class SignatureChecker(
         while (i > 0) {
             p = pointAddition(p, p, primeNumber, a)
             if (bits[bits.length - i] == '1') {
-                p = pointAddition(p, point, primeNumber, a)
+                p = pointAddition(point, p, primeNumber, a)
             }
             i--
         }
         return p
     }
 
-    fun pointAddition(
-        p1: EllipticCurvePoint,
-        p2: EllipticCurvePoint,
-        primeNumber: Long,
-        a: Long
-    ): EllipticCurvePoint {
-        if (p2.pointAtInfinity) {
-            return p1
-        }
-        if (p1.pointAtInfinity) {
-            return p2
-        }
-        try {
-            var lambda = if (p1.isEq(p2)) {
-                (3 * (p1.x) * (p1.x) + a) * invMod(2 * p1.y, primeNumber)
-            } else {
-                (p2.y - p1.y) * invMod(p2.x - p1.x, primeNumber)
+    companion object {
+        fun pointAddition(
+            p1: EllipticCurvePoint,
+            p2: EllipticCurvePoint,
+            primeNumber: Long,
+            a: Long
+        ): EllipticCurvePoint {
+            if (p2.pointAtInfinity) {
+                return p1
             }
-            val x3 = (lambda * lambda - p1.x - p2.x)
+            if (p1.pointAtInfinity) {
+                return p2
+            }
+            val lambda: Long
+            if (mod(p1.x - p2.x, primeNumber) == 0L) {
+                if (mod(p1.y - p2.y, primeNumber) == 0L) {
+                    lambda = (3 * (p1.x) * (p1.x) + a) * invMod(2 * p1.y, primeNumber)
+                } else {
+                    return EllipticCurvePoint(0, 0, true)
+                }
+            } else {
+                lambda = mod((p2.y - p1.y), primeNumber) * invMod(p2.x - p1.x, primeNumber)
+            }
 
-            return EllipticCurvePoint(
-                Math.floorMod(x3 , primeNumber),
-                Math.floorMod(lambda * (p1.x - x3) - p1.y, primeNumber),
-                false
-            )
-        } catch (e: Exception) {
-//            println(String.format("bam"))
-            return EllipticCurvePoint(0, 0, true)
+            val x3 = mod(lambda * lambda - p1.x - p2.x, primeNumber)
+            val y3 = mod((lambda * (p1.x - x3) - p1.y), primeNumber)
+            return EllipticCurvePoint(x3, y3, false)
+        }
+        fun pointSub(
+            p1: EllipticCurvePoint,
+            p2: EllipticCurvePoint,
+            primeNumber: Long,
+            a: Long
+        ): EllipticCurvePoint{
+            if (p1.isEq(p2)) return EllipticCurvePoint(0, 0, true)
+            return pointAddition(EllipticCurvePoint(p2.x, -p2.y, false), p1, primeNumber, a)
+        }
+
+        private fun mod(dividend: Long, divisor: Long): Long {
+            var nB1: BigInteger = BigInteger.valueOf(dividend)
+            var nB2: BigInteger = BigInteger.valueOf(divisor)
+            return nB1.mod(nB2).longValueExact()
+        }
+        private fun invMod(
+            n1: Long,
+            n2: Long
+        ): Long {
+            var nB1: BigInteger = BigInteger.valueOf(n1)
+            var nB2: BigInteger = BigInteger.valueOf(n2)
+            return nB1.modInverse(nB2).longValueExact()
         }
     }
 }
